b0c32d85a90ceab2b053c735edfdb32b2f66cc35
rosera <askrichardrose@gmail.com>
2023-09-27 07:36:03 +0100

    Add: updated zonal documentation

--- random_zone/main.tf
+++ random_zone/main.tf
@@ -1,9 +1,11 @@
+## Create a randomised Zonal list
+
 # 1. Get the zones for the Region
 data "google_compute_zones" "available_zones" {
   region = var.gcp_region
 }
 
-# 2. Define a index based on available_zones
+# 2. Define an index based on available_zones
 resource random_integer "random_index" {
   min = 0
   max = length(data.google_compute_zones.available_zones.names) -1
@@ -11,11 +13,13 @@ resource random_integer "random_index" {
 
 # 3. Create a filtered zone list - minus the current QL allocated zone
 locals {
+  # Option 1. For Loop
   filtered_zones =  [
   for zone in data.google_compute_zones.available_zones.names :
     zone if zone != var.gcp_zone
   ]
 
+  # Option 2. setsubtract
   filtered_zones2 = setsubtract(data.google_compute_zones.available_zones.names, [var.gcp_zone])
 
   random_zone  = local.filtered_zones[random_integer.random_index.result]
