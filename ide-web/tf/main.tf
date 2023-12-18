3ca8ef50e89e8e9303d744ad36ea66fc8ae55e10
rosera <askrichardrose@gmail.com>
2023-12-18 15:45:48 +0000

    Add: Post merge updates

---
+++ ide-web/tf/main.tf
@@ -0,0 +1,42 @@
+# ------------------ IDE: Integrated Development Environment   
+# ------------------ Module Definition 
+#
+
+# Local:  modules/[channel]
+# Remote: github.com://CloudVLab/terraform-lab-foundation//[module]/[channel]
+
+# Solution: IDE environment 
+# Local:  modules/stable
+# Remote: github.com/CloudVLab/terraform-lab-foundation//solutions/ide_web/stable
+
+# Output Value(s):
+# - ideEditorService  : URL of IDE Service
+# - ideBrowserService : URL of Browser Service
+# - ideInstanceName   : URL of Browser Service
+
+module "la_ide" {
+  ## NOTE: When changing the `source` parameter
+  ## `terraform init` is required
+
+  ## Local Modules - working
+  ## Module subdirectory needs to be defined within the TF directory
+  # source = "./solutions/ide_web/stable"
+
+  ## REMOTE: GitHub (Public) access - working 
+  source = "github.com/CloudVLab/terraform-lab-foundation//solutions/ide_web/dev?ref=tlf_ide"
+  # source = "github.com/CloudVLab/terraform-lab-foundation//solutions/ide_web/stable"
+
+  ## Exchange values between Qwiklabs and Module
+  gcp_project_id = var.gcp_project_id
+  gcp_region     = var.gcp_region
+  gcp_zone       = var.gcp_zone
+
+  ## Properties: GCE 
+  gceMachineImage = "test-flutter-codeserver"
+  # gceMachineImage = "flutter-codeserver"
+  gceDiskSize     = 100
+
+  ## Properties: Cloud Run
+  gcrRegion = var.gcp_region
+}
+
