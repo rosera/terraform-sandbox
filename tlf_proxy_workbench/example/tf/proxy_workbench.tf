15295fe828255f4e103656c621b944eb3264b933
rosera <askrichardrose@gmail.com>
2025-03-13 10:55:19 +0000

    Add: Template + Folder rename

--- tlf_proxy_workbench/example/tf/proxy_workbench.tf
+++ tlf_proxy_workbench/example/tf/proxy_workbench.tf
@@ -4,7 +4,8 @@
 module "la_vertex_workbench" {
   ## REMOTE: GitHub (Public) access - working 
   ## source = "github.com/CloudVLab/terraform-lab-foundation//solutions/proxy_vertex_workbench/stable"
-  source = "github.com/CloudVLab/terraform-lab-foundation//solutions/proxy_vertex_workbench/stable/v1"
+  ## source = "github.com/CloudVLab/terraform-lab-foundation//solutions/proxy_vertex_workbench/stable/v1"
+  source = "gcs::https://www.googleapis.com/storage/v1/terraform-lab-foundation/solutions/proxy_vertex_workbench/stable/v1"
 
   ## Exchange values between Qwiklabs and Module
   gcp_project_id  = var.gcp_project_id 
@@ -12,8 +13,8 @@ module "la_vertex_workbench" {
   gcp_zone        = var.gcp_zone 
 
   ## Custom Properties
-  # workbench_name                = "cls-vertex-workbench"
-  # workbench_service_name        = "workbench-proxy"
+  workbench_name                = "cls-workbench"
+  workbench_service_name        = "cls-proxy"
   # workbench_proxy_image         = "gcr.io/qwiklabs-resources/workbench-proxy:latest"
   # workbench_sa_name             = "vertex-ai"
   # workbench_sa_description      = "Vertex Workbench lab service account"
